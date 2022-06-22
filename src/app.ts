dc37be399a5a01b14f26fc5406b942db8569cfb7
trofimovby <trofas03@yandex.ru>
2022-06-22 11:35:33 +0300

    two

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.info("hello")
+import {Telegraf} from 'telegraf'
+
+const bot = new Telegraf("5400151287:AAGMaErASVaY_QMcYvmqxeX6CI3bsyCgUUY")
+bot.start((ctx) => ctx.reply('Здарова заебал'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Здарова заебал'))
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
